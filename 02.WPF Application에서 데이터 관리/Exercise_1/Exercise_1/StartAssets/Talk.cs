9d8a03beced7e77701f42f4fef8fdb628bc41c36
gijung2 <gijung2@inavi.kr>
2021-10-26 01:49:39 +0900

    데이터 바인딩 추가
    1. 데이터 바인딩 내용 추가

---
+++ 02.WPF Application에서 데이터 관리/Exercise_1/Exercise_1/StartAssets/Talk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Exercise_1.StartAssets
+{
+    class Talk : ObservableCollection<Message>
+    {
+        public Talk()
+        {
+            this.Add(new Message() { Sender = "Adventure Works", Content = "Hi, What Can we do for you?" });
+            this.Add(new Message() { Sender = "Client", Content = "Did you receive the GR268 KZ bike?" });
+        }
+    }
+
+    class Message
+    {
+        public string Sender { get; set; }
+        public string Content { get; set; }
+    }
+
+}
---
+++ 02.WPF Application에서 데이터 관리/WpfDataBinding/WpfDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDataBinding
+{
+
+    class Car
+    {
+        public int Speed { get; set; }
+
+        public Color Color { get; set; }
+
+        public Car(int speed, Color color)
+        {
+            Speed = speed;
+            Color = color;
+        }
+    }
+
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Car> list = new List<Car>()
+            {
+                new Car(1, Color.FromRgb(255, 0, 0)),
+                new Car(2, Color.FromRgb(255, 0, 0)),
+                new Car(3, Color.FromRgb(255, 0, 0)),
+            };
+
+            this.DataContext = list;
+        }
+    }
+}
